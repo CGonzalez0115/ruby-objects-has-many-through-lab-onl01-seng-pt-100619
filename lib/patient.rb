84e1559d7982091dc52f292140022bc4a0fa6e29
Christina Gonzalez <cmgamway@gmail.com>
2020-01-16 00:54:16 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,7 +16,8 @@ class Patient
   end
 
   def appointments
-    Appointment.all.select {|appointment| appointment.patient == self}
+    appointment = Appointment.all.select do |patient|
+      appointment.patient == self
   end
 
   def doctors
